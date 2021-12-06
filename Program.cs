2f4cee884f6c231bad66fe738e092cebd917a7c8
elegantdev <94651811+elegantdev@users.noreply.github.com>
2021-12-06 15:11:38 +1100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,76 +1,140 @@
- AccountController:
-     public ActionResult LogOn(LogOnModel model, string returnUrl)
+  "AzurePubSubSettings": {
+    "connectionString": "Endpoint=https://pubsubinital.webpubsub.azure.com;AccessKey=E/QKZYo0iygWd2a+leRmu0FLI7DMps9Oqf/xjrV2GGo=;Version=1.0;"
+  },
 
--->
-	 var auth = new AuthenticateUser();
 
-               bool hasAccess = auth.getUserAccess(model.UserName);
 
-                if(!hasAccess)
-                    return Redirect("http://www.google.com");
-					
-					
-	~~~~
-AuthenticateUser.cs
--->	
 
-     public bool getUserAccess(string username)
+
+using Azure.Messaging.WebPubSub;
+using FrontierCoreAPI.Application.Interfaces;
+using FrontierCoreAPI.Domain.Settings;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+
+namespace FrontierCoreAPI.Infrastructure.Shared.Services
+{
+    public class AzurePubSubService : IAzurePubSubService
+    {
+        public AzurePubSubSettings _azurePubSubSettings { get; }
+        public ILogger<AzurePubSubService> _logger { get; }
+
+        public AzurePubSubService(IOptions<AzurePubSubSettings> azurePubSubSettings, ILogger<AzurePubSubService> logger)
         {
-            CloudTable tbl = AuthTable();
-
-            var entities = tbl.ExecuteQuery(new TableQuery<Users>()).ToList().Where(x=> x.mail == username).FirstOrDefault();
-            
-            if(entities != null)
-                return entities.accessMercury;
-
-            return false;
-           
+            _azurePubSubSettings = azurePubSubSettings.Value;
+            _logger = logger;
         }
-
-        private static CloudTable AuthTable()
+        /// <summary>
+        /// Publish message to websocket stream of azure
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="channel"></param>
+        /// <returns></returns>
+        public async Task<bool> SendNotification(string message, string channel)
         {
-            string accountName = "initialdbstorage";
-            string accountKey = "OlJgQ4W/J3aW91v9v1weXs6mq8Pmigl2RfWxqlczcK2VUhiOzCge9VOL+NNe4dvp/O5cyY/FwpP0mkCQ==";
+
+            _logger.LogInformation($"SendNotification channel:{channel} message:{message} ");
+
+            if (string.IsNullOrEmpty(channel))
+                channel = "Hub"; // default channel name
+
+            //Create the service client
+            var serviceClient = new WebPubSubServiceClient(_azurePubSubSettings.connectionString, channel);
+
             try
             {
-                StorageCredentials creds = new StorageCredentials(accountName, accountKey);
-                CloudStorageAccount account = new CloudStorageAccount(creds, useHttps: true);
-                CloudTableClient client = account.CreateCloudTableClient();
-                CloudTable table = client.GetTableReference("initaltable");
+                //Publish message in websocket stream
+                await serviceClient.SendToAllAsync(message);
 
-                return table;
             }
-            catch
+            catch (Exception ex)
             {
-                return null;
+                _logger.LogError($"SendNotification channel:{channel} message:{message} Error:{ex.Message} stackTrace:{ex.StackTrace}");
+
+                throw ex;
+
+            }
+            return true;
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+using FrontierCoreAPI.Application.Interfaces;
+using FrontierCoreAPI.Application.Wrappers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrontierCoreAPI.Application.Features.AzurePubSub.Commands.SendNotification
+{
+    public class SendNotificationCommand : IRequest<Response<bool>>
+    {
+        public string SenderName { get; set; }
+        public string Message { get; set; }
+        public string Channel { get; set; }
+
+        public class SendNotificationCommandHandler : IRequestHandler<SendNotificationCommand, Response<bool>>
+        {
+            private readonly IAzurePubSubService _azurePubSubService;
+
+            //
+            public SendNotificationCommandHandler(IAzurePubSubService azurePubSubService)
+            {
+                _azurePubSubService = azurePubSubService;
+            }
+
+
+            public async Task<Response<bool>> Handle(SendNotificationCommand command, CancellationToken cancellationToken)
+            {
+               await _azurePubSubService.SendNotification(command.Message,command.Channel);
+
+                return new Response<bool>(true, $"Message broadcasted{(string.IsNullOrEmpty(command.Channel) ? "" :  " in " + command.Channel)} successfully!");
             }
         }
-		
-		
-		
-				
 
---> Users.cs
-
-  public class Users : TableEntity
-    {
-        public Users() { }
-        public bool accessBackOffice { get; set; }
-        public bool accessGlide { get; set; }
-        public bool accessManagerAnalytics { get; set; }
-        public bool accessPeerComparison { get; set; }
-        public bool accessPortal { get; set; }
-        public bool accessPortfolioAnalytics { get; set; }
-        public bool accessPreRegister { get; set; }
-        public bool accessRadias { get; set; }
-        public bool accessUserDocs { get; set; }
-        public string familyName { get; set; }
-        public string givenName { get; set; }
-        public bool is_admin { get; set; }
-        public string mail { get; set; }
-        public bool accessPreviewPortfolioAnalytics { get; set; }
-        public bool accessMercury { get; set; }
-       
     }
+}
 
-	
+
+
+
+
+
+
+
+
+using FrontierCoreAPI.Application.Features.AzurePubSub.Commands.SendNotification;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FrontierCoreAPI.WebApi.Controllers.v1
+{
+    [ApiVersion("1.0")]
+
+    public class AzurePubSubController : BaseApiController
+    {
+        /// <summary>
+        /// Publish message 
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(SendNotificationCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
